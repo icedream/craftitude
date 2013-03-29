994a091f4f98d0c97991ed562d223d46a619f7b0
Icedream <icedream2k9@die-optimisten.net>
2013-03-29 23:02:20 +0100

    Add initial source files.

---
+++ source/client_api/MinovaClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Minova.ClientApi
+{
+    public class MinovaClient
+    {
+        public Uri RepositoryUrl { get; private set; }
+        internal Uri RepositoryEnhancementsUrl { get { return new Uri(RepositoryUrl, "enhanced/"); } }
+
+        private string _targetDirectory = Environment.CurrentDirectory;
+        public string TargetDirectory { get { return _targetDirectory; } set { _targetDirectory = value; } }
+        internal DirectoryInfo TargetDirectoryInfo { get { return new DirectoryInfo(_targetDirectory); } }
+        internal DirectoryInfo MinovaDirectoryInfo { get; private set; }
+        internal string MinovaRepositoriesListFilePath { get { return Path.Combine(MinovaDirectoryInfo.FullName, "repositories.list"); } }
+
+        private List<string> _repositories = new List<string>(new[] {
+            "http://repo.minova.tk/minecraft/",
+            "http://repo.minova.tk/libraries/",
+            "http://repo.minova.tk/mods/"
+        });
+        public List<string> Repositories { get { return _repositories; } }
+
+        private void _internalTargetInit()
+        {
+            MinovaDirectoryInfo = TargetDirectoryInfo.CreateSubdirectory("minova");
+            if (!File.Exists(MinovaRepositoriesListFilePath))
+                _saveRepositoriesList();
+            else
+                _loadRepositoriesList();
+        }
+
+        public void Save()
+        {
+            _saveRepositoriesList();
+        }
+
+        private void _saveRepositoriesList()
+        {
+            File.WriteAllLines(MinovaRepositoriesListFilePath, Repositories);
+        }
+
+        private void _loadRepositoriesList()
+        {
+            Repositories.Clear();
+            Repositories.AddRange(File.ReadAllLines(MinovaRepositoriesListFilePath));
+        }
+    }
+}
---
+++ source/client_api/RemoteRepository.cs
@@ -0,0 +1,128 @@
+﻿#region Imports (14)
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using YamlDotNet;
+using YamlDotNet.Core;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.RepresentationModel.Serialization;
+
+#endregion Imports (14)
+
+namespace Minova.ClientApi
+{
+    internal class Distribution
+    {
+        #region Properties of Distribution (3)
+
+        public string DistributionID { get; private set; }
+
+        internal Uri DistributionUrl { get { return Repository.GetUri(string.Format("{0}/", DistributionID)); } }
+
+        internal Repository Repository { get; private set; }
+
+        #endregion Properties of Distribution (3)
+
+        #region Constructors of Distribution (1)
+
+        public Distribution(Repository parentRepository, string distributionName)
+        {
+            Repository = parentRepository;
+            DistributionID = distributionName;
+
+            // TODO: Repository validation with exception on fail
+        }
+
+        #endregion Constructors of Distribution (1)
+
+        #region Methods of Distribution (1)
+
+        internal Uri GetUri(string relativePath)
+        {
+            return new Uri(DistributionUrl, relativePath);
+        }
+
+        public DistributionPackageList GetAvailablePackages(string since)
+        {
+            YamlSerializer s = new YamlSerializer(typeof(DistributionPackageList));
+            HttpWebRequest wr = (HttpWebRequest)HttpWebRequest.Create(GetUri("packages.yml?since=" + since));
+            var wresp = wr.GetResponse();
+            DistributionPackageList ret = null;
+            using (var ws = wresp.GetResponseStream())
+            {
+                using (var wsr = new StreamReader(ws))
+                {
+                    ret = (DistributionPackageList)s.Deserialize(wsr);
+                }
+            }
+            return ret;
+        }
+
+        #endregion Methods of Distribution (1)
+    }
+
+    public class DistributionPackageList : List<DistributionPackageListEntry>
+    {
+    }
+
+    public class DistributionPackageListEntry
+    {
+        public string CurrentVersion { get; internal set; }
+
+        public PackageMetadata Metadata { get; internal set; }
+    }
+
+    public class PackageMetadata
+    {
+        public string[] Dependencies { get; private set; }
+
+        public string Description { get; private set; }
+
+        public string[] Developers { get; private set; }
+
+        public string ID { get; internal set; }
+
+        public string License { get; private set; }
+
+        [YamlIgnore()]
+        public string LicenseNameOrText { get { return string.Join("; ", License.Split(new string[] { "; " }, StringSplitOptions.RemoveEmptyEntries).Where(licenseEntry => !Uri.IsWellFormedUriString(licenseEntry, UriKind.Absolute))); } }
+
+        [YamlIgnore()]
+        public string LicenseUrl { get { return License.Split(new string[] { "; " }, StringSplitOptions.RemoveEmptyEntries).SingleOrDefault(licenseEntry => Uri.IsWellFormedUriString(licenseEntry, UriKind.Absolute)); } }
+
+        public string[] Maintainers { get; private set; }
+
+        public string Name { get; private set; }
+
+        public bool PlatformDependence { get; private set; }
+    }
+
+    internal class Repository
+    {
+        #region Properties of Repository (1)
+
+        public Uri RepositoryUrl { get; private set; }
+
+        #endregion Properties of Repository (1)
+
+        #region Methods of Repository (2)
+
+        public Distribution GetDistribution(string dist)
+        {
+            return new Distribution(this, dist);
+        }
+
+        internal Uri GetUri(string relativePath)
+        {
+            return new Uri(RepositoryUrl, relativePath);
+        }
+
+        #endregion Methods of Repository (2)
+    }
+}
