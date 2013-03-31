e550639310f645e047a6b825627e50c93efe2dda
Icedream <icedream2k9@die-optimisten.net>
2013-03-31 21:22:37 +0200

    Added incomplete PackageInstructionsScript class.

---
+++ source/client_api/PackageInstructionsScript.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LuaInterface;
+
+namespace Craftitude.ClientApi
+{
+    using Archives;
+    using Resolvers;
+
+    class PackageInstructionsScript : Lua
+    {
+        public PackageInstructionsScript()
+        {
+
+        }
+
+        internal ArchiveBase _sourcePackage;
+        internal PackageMetadata _packageMetadata;
+
+        private void UnpackAllFromSource(string targetdir)
+        {
+            _sourcePackage.ExtractAllFiles(targetdir);
+        }
+
+        private void UnpackAllFromSource(string targetdir, string filter)
+        {
+            // TODO: Implement UnpackAllFromSource with filter
+        }
+
+        private void InjectAllFromSource(string targetfile)
+        {
+            // TODO: Implement InjectAllFromSource
+        }
+
+        private void InjectAllFromSource(string targetfile, string filter)
+        {
+            // TODO: Implement UnpackAllFromSource with filter
+        }
+
+        private void DeleteFile(string targetfile)
+        {
+            // TODO: Implement DeleteFile
+        }
+
+        private void DeleteDir(string targetfile)
+        {
+            // TODO: Implement DeleteDir
+        }
+
+        private void Move(string source, string target)
+        {
+            // TODO: Implement Move
+        }
+
+        private void Rename(string source, string target)
+        {
+            Move(source, target);
+        }
+
+        private object GetPackageMetadata(string propertyname)
+        {
+            return _packageMetadata.Data[propertyname];
+        }
+
+        private object GetPackageVersion()
+        {
+            // TODO: Implement GetPackageVersion
+            throw new NotImplementedException();
+        }
+
+        private ResolverBase GetResolver(string name)
+        {
+            return GetResolver(name, new Hashtable());
+        }
+
+        private ResolverBase GetResolver(string name, Hashtable parameters)
+        {
+            var resolver = ResolverUtil.GetResolverByName(name);
+            resolver.Parameters = parameters;
+            return resolver;
+        }
+    }
+}
